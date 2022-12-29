7becc963935f249995c32e28cdb33860e0f910d8
Marcus Ayers <marcayers27@gmail.com>
2022-12-29 16:25:42 -0500

    final

---
+++ app 2/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def property
+    @data = { property_id: params[:id] }.to_json
+    render 'property'
+  end
+
+  def login
+    render 'login'
+  end
+  
+end
