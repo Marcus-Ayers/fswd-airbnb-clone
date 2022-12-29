39294fe861e013c3de33496f30b08a6527505512
Marcus Ayers <marcayers27@gmail.com>
2022-12-29 11:01:53 -0500

    changes --unfinshed

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,19 @@ Rails.application.routes.draw do
   get '/property/:id' => 'static_pages#property'
   get '/login' => 'static_pages#login'
 
-
   namespace :api do
     # Add routes below this line
     resources :users, only: [:create]
     resources :sessions, only: [:create, :destroy]
     resources :properties, only: [:index, :show]
+    resources :bookings, only: [:create]
+    resources :charges, only: [:create]
 
     get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
 
+    # stripe webhook
+    post '/charges/mark_complete' => 'charges#mark_complete'
   end
 
 end
